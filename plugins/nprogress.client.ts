27258f32af5a390a429c1f3e699df3e2ea582a66
miqayel-gubryan-s2s <miqayel.gubryan@s2s.am>
2025-07-20 21:10:58 +0400

    Initial commit

--- plugins/nprogress.client.ts
+++ plugins/nprogress.client.ts
@@ -1,6 +1,9 @@
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
+NProgress.configure({ showSpinner: false })
+
+
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.hook('page:start', () => {
 		NProgress.start()
