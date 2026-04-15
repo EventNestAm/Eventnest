21d819f6b97a3116819a3c2eacee33e12132306e
Your Name <you@example.com>
2026-04-15 12:53:31 +0400

    Code review

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -249,7 +249,7 @@ export function useEvents() {
       description:
         t("KARGIN_QUIZ_TITLE"),
       category: t("QUIZ"),
-      eventDate: true,
+      eventDate: false,
       emailSent: true, groupName: true
     },
     {
