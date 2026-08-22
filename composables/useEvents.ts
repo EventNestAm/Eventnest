c29822c312955a1fba2f58bf3e674d46f95788f2
Your Name <your-email@example.com>
2026-08-22 04:35:41 +0400

    LAST

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -974,7 +974,24 @@ export function useEvents() {
       emailSent: false, groupName: true, isSoldout: false, quantity: 45,
       pricePerPerson: 3000
     },
-
+    {
+      id: 56,
+      title: `Կարգին հաղորդման ${t("TOURNAMENT")} #7`,
+      slug: "kargin-haxordman-viktorina-21",
+      titleDesc: "An evening of strategy, emotions and people like you",
+      date: "2026-08-29",
+      dateTitle: "Օգոստոսի 29",
+      time: "19:30",
+      price: "3000 AMD",
+      location: "The Kond House - 46 Leo Street",
+      image: Kargin2,
+      description:
+        t("KARGIN_QUIZ_TITLE"),
+      category: [t("QUIZ"), t("KARGIN")],
+      eventDate: true,
+      emailSent: false, groupName: true, isSoldout: false, quantity: 45,
+      pricePerPerson: 3000
+    },
   ])
   const eventsWithStatus = computed(() =>
     events.value.map(event => {
