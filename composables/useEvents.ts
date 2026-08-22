0dbe11bf882cc9fb38e440c8296726587e6ce161
Your Name <your-email@example.com>
2026-08-22 13:18:02 +0400

    LAST

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -898,7 +898,7 @@ export function useEvents() {
         t("KARGIN_QUIZ_TITLE"),
       category: [t("QUIZ"), t("KARGIN")],
       eventDate: true,
-      emailSent: true, groupName: true, isSoldout: false, quantity: 15,
+      emailSent: true, groupName: true, isSoldout: false, quantity: 10,
       pricePerPerson: 3000
     },
     {
@@ -916,7 +916,7 @@ export function useEvents() {
         t("KARGIN_SERIAL_TITLE"),
       category: [t("QUIZ"), t("KARGIN")],
       eventDate: true,
-      emailSent: true, groupName: true, quantity: 12,
+      emailSent: true, groupName: true, quantity: 10,
       pricePerPerson: 3000
     },
     {
@@ -953,7 +953,7 @@ export function useEvents() {
         t("KARGIN_QUIZ_TITLE"),
       category: [t("QUIZ"), t("KARGIN")],
       eventDate: true,
-      emailSent: false, groupName: true, isSoldout: false, quantity: 45,
+      emailSent: true, groupName: true, isSoldout: false, quantity: 45,
       pricePerPerson: 3000
     },
     // {
