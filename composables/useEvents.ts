7f819167dc4e91a5e2892c570d5a5049b3328a46
Your Name <your-email@example.com>
2026-08-23 14:01:23 +0400

    LAST

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -956,42 +956,24 @@ export function useEvents() {
       emailSent: true, groupName: true, isSoldout: false, quantity: 45,
       pricePerPerson: 3000
     },
-    // {
-    //   id: 55,
-    //   title: `Կարգին հաղորդման ${t("TOURNAMENT")} #6`,
-    //   slug: "kargin-haxordman-viktorina-20",
-    //   titleDesc: "An evening of strategy, emotions and people like you",
-    //   date: "2026-08-29",
-    //   dateTitle: "Օգոստոսի 29",
-    //   time: "19:30",
-    //   price: "3000 AMD",
-    //   location: "The Kond House - 46 Leo Street",
-    //   image: Kargin2,
-    //   description:
-    //     t("KARGIN_QUIZ_TITLE"),
-    //   category: [t("QUIZ"), t("KARGIN")],
-    //   eventDate: true,
-    //   emailSent: false, groupName: true, isSoldout: false, quantity: 45,
-    //   pricePerPerson: 3000
-    // },
-    // {
-    //   id: 56,
-    //   title: `Կարգին հաղորդման ${t("TOURNAMENT")} #8`,
-    //   slug: "kargin-haxordman-viktorina-21",
-    //   titleDesc: "An evening of strategy, emotions and people like you",
-    //   date: "2026-08-29",
-    //   dateTitle: "Օգոստոսի 29",
-    //   time: "19:30",
-    //   price: "3000 AMD",
-    //   location: "The Kond House - 46 Leo Street",
-    //   image: Kargin2,
-    //   description:
-    //     t("KARGIN_QUIZ_TITLE"),
-    //   category: [t("QUIZ"), t("KARGIN")],
-    //   eventDate: true,
-    //   emailSent: false, groupName: true, isSoldout: false, quantity: 45,
-    //   pricePerPerson: 3000
-    // },
+    {
+      id: 55,
+      title: `${t("KARAOKE_BATTLE_TITLE")} #6`,
+      slug: "karaoke-battle-6",
+      titleDesc: "An evening of strategy, emotions and people like you",
+      date: "2026-08-27",
+      dateTitle: "Օգոստոսի 27",
+      time: "19:30",
+      price: "4000 AMD",
+      location: "The Kond House - 46 Leo Street",
+      image: karaokeBattle,
+      description:
+        t("KARAOKE_BATTLE"),
+      category: t("KARAOKE"),
+      eventDate: true,
+      emailSent: true, groupName: false, isSoldout: false, quantity: 20,
+      pricePerPerson: 4000
+    },
   ])
   const eventsWithStatus = computed(() =>
     events.value.map(event => {
