0e0e8fb3aa0803c34a71a18019436d1d3fe8fd9b
Your Name <your-email@example.com>
2026-09-08 18:23:59 +0400

    LAST

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -1002,7 +1002,7 @@ export function useEvents() {
         t("MUSIC_TITLE"),
       category: [t("QUIZ"), t("MUSIC")],
       eventDate: true,
-      emailSent: false, groupName: true, isSoldout: false, quantity: 28,
+      emailSent: false, groupName: true, isSoldout: false, quantity: 21,
       pricePerPerson: 4000
     },
     {
@@ -1019,8 +1019,8 @@ export function useEvents() {
         t("GAME_OF_THRONES_QUIZ_DESC"),
       category: [t("QUIZ"), t("MOVIES")],
       eventDate: true,
-      emailSent: false, groupName: true, isSoldout: false, quantity: 35,
-      pricePerPerson: 3000
+      emailSent: false, groupName: true, isSoldout: false, quantity: 24,
+      pricePerPerson: 4000
     },
     {
       id: 59,
