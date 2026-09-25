4fda8521e52843eae182bb2aa0302e2b9d001429
Your Name <your-email@example.com>
2026-09-25 17:04:30 +0400

    LAST

--- composables/useEvents.ts
+++ composables/useEvents.ts
@@ -1090,23 +1090,23 @@ export function useEvents() {
       emailSent: false, groupName: true, isSoldout: false, quantity: 25,
       pricePerPerson: 4000
     },
-    // {
-    //   id: 60,
-    //   title: `${t("MEME")} ${t("QUIZ_TITLE")} #2`,
-    //   slug: "meme-quiz-2",
-    //   titleDesc: "An evening of emotions and people like you",
-    //   date: "2026-09-17",
-    //   time: "19:30",
-    //   price: "3000 AMD",
-    //   location: "Shamrock Yvn - Ghazar Parpetsi 26",
-    //   image: "/images/events/memequiz.jpg",
-    //   description:
-    //     t("MEME_DESC"),
-    //   category: [t("QUIZ")],
-    //   eventDate: true,
-    //   emailSent: false, groupName: true, isSoldout: false, quantity: 35,
-    //   pricePerPerson: 3000
-    // },
+    {
+      id: 63,
+      title: `${t("MEME")} ${t("QUIZ_TITLE")}`,
+      slug: "meme-quiz-2",
+      titleDesc: "An evening of emotions and people like you",
+      date: "2026-10-03",
+      time: "19:30",
+      price: "4000 AMD",
+      location: "The Kond House - 46 Leo Street",
+      image: "/images/events/memesquiz.jpg",
+      description:
+        t("MEME_DESC"),
+      category: [t("QUIZ")],
+      eventDate: true,
+      emailSent: false, groupName: true, isSoldout: false, quantity: 35,
+      pricePerPerson: 4000
+    },
   ])
   const eventsWithStatus = computed(() =>
     events.value.map(event => {
